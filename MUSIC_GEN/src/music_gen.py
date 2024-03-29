3df93af463cb33294dfc01703df213461d9e91f4
CodewithAbhi7 <112254825+CodewithAbhi7@users.noreply.github.com>
2024-03-29 22:34:34 +0530

    Add files via upload

---
+++ MUSIC_GEN/src/music_gen.py
@@ -0,0 +1,54 @@
+import streamlit as st
+from transformers import pipeline
+import scipy
+import os
+import wave
+
+# Load MusicGen pipeline
+synthesiser = pipeline("text-to-audio", "facebook/musicgen-small")
+
+MUSIC_FOLDER = "../music"  # Name of the folder to save audio files
+
+def create_audio(user_input, file_path):
+        with st.spinner("Generating audio..."):
+            music = synthesiser(user_input, forward_params={"do_sample": True})
+            audio_data = music["audio"]
+            sampling_rate = music["sampling_rate"]
+            scipy.io.wavfile.write(file_path, rate=sampling_rate, data=audio_data)
+            st.success(f"Audio generated and saved as {file_path}.")
+
+def app():
+    st.title("MusicGen Streamlit App")
+
+    user_input = st.text_area("Enter music description (e.g., 'lo-fi music with a soothing melody'):")
+    
+    # User input for the desired filename
+    filename = st.text_input("Enter the desired music filename (without extension):", "musicgen_out")
+
+    if st.button("Generate Audio"):
+        if user_input:
+
+            # Ensure the 'music' folder exists
+            os.makedirs(MUSIC_FOLDER, exist_ok=True)
+            
+            file_path = os.path.join(MUSIC_FOLDER, f"{filename}.wav")
+            
+            create_audio(user_input, file_path)
+
+            # Play audio in the app
+            st.audio(file_path, format="audio/wav")
+
+    
+    st.header("Play generated Audio Files")
+
+    # Display a list of available audio files in the 'music' folder
+    audio_files = os.listdir(MUSIC_FOLDER)
+    selected_file = st.selectbox("Select an audio file to play:", audio_files, index=0)
+
+    if selected_file:
+        selected_file_path = os.path.join(MUSIC_FOLDER, selected_file)
+        st.audio(selected_file_path, format="audio/wav", start_time=0)
+
+
+if __name__ == "__main__":
+    app()
---
+++ README.md
@@ -0,0 +1,23 @@
+# Music-generator-webapp
+
+## run the code
+
+Download the MUSIC_GEN folder to a folder of you choice
+
+Open the folder in terminal and in the terminal do following:
+
+Create python virtual environment and activate it:
+
+python3 -m venv venv/		# make venv called venv
+
+source venv/bin/activate	# activate venv
+
+Install the requirements:
+
+pip install -r requirements.txt # install requirements
+
+Navigate to the src folder
+
+run the app:
+
+streamlit run music_gen.py
